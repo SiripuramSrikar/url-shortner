6805850cb19038d2eaa3a5862927ef6d434c5905
SiripuramSrikar <siripuramsrikar100705@gmail.com>
2025-02-07 18:37:33 +0530

    Home Page Completed

--- app/page.js
+++ app/page.js
@@ -1,9 +1,9 @@
 import Image from "next/image";
 import localFont from "next/font/local"
 
-const geistSans = localFont({
+const poppins = localFont({
   src:"./fonts/Poppins-Bold.ttf",
-  variable:"--font-geist-sans",
+  variable:"--font-poppins",
   weight:"100 900",
 })
 export default function Home() {
@@ -11,8 +11,14 @@ export default function Home() {
     <main className="bg-purple-100">
       <section className="grid grid-cols-2 h-[50vh]">
         <div className="flex flex-col gap-4 items-center justify-center">
-          <p className="text-2xl font-bold">The best URL Shortner in the market</p>
-          <p>We are the most striaght forward URL Shortner in the world</p> 
+          <p className={`text-3xl font-bold ${poppins.className}`}>The best URL Shortner in the market</p>
+          <p className="px-40 text-center">We are the most striaght forward URL Shortner in the world. Most of the url 
+            shortners will track you or ask to give your details for login. We 
+            understand your needs and hence we have created this URL Shortner</p> 
+            <div className='flex gap-3 justify-start'>
+                <a href='/generate'><button className='bg-black rounded-xl shadow-lg p-3 py-1 font-bold text-white'>Try Now</button></a>
+                <a href='/github'><button className='bg-black  rounded-xl  py-1 font-boldshadow-lg p-3 py-1 font-bold text-white'>Github</button></a>
+            </div>
         </div>
         <div className="flex justify-start relative">
           <Image className="mix-blend-darken" alt="vector" src={"/vector.png"} fill={true}></Image>
