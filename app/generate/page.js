d750e470de2b1d18193acc5fc54a764ee2a49316
SiripuramSrikar <siripuramsrikar100705@gmail.com>
2025-02-07 19:20:30 +0530

    Shorten Page Completed

---
+++ app/generate/page.js
@@ -0,0 +1,23 @@
+"use client"
+import React, {useState} from 'react'
+
+const Shorten = () => {
+    const [url, seturl] = useState("")
+    const [shorturl, setshorturl] = useState("")
+    const [Generated, setGenerated] = useState(false)
+
+  return (
+    <div className='mx-auto max-w-lg bg-green-100 my-16 p-8 rounded-xl flex-col gap-5'>
+        <h1 className='font-bold text-2xl'>
+            Generate your short URLs
+        </h1>
+        <div className='flex flex-col gap-2'>
+            <input type='text' value={url} placeholder='Enter your URL' onChange={e=>{seturl(e.target.value)}} className=' p-4 rounded-md'></input>
+            <input type='text' value={shorturl} className='p-4 focus:outline-blue-800 rounded-md' placeholder='Enter your preferred short URL text'onChange={e=>{setshorturl(e.target.value)}}></input>
+            <button className='bg-black text-white rounded-md shadow-lg p-3 py-1 my-3 font-bold'>Generate</button>
+        </div>
+    </div>
+  )
+}
+
+export default Shorten
--- components/Navbar.js
+++ components/Navbar.js
@@ -9,7 +9,6 @@ export const Navbar = () => {
             <a href='/'><li>Home</li></a>
             <a href='/about'><li>About</li></a>
             <a href='/contact'><li>Contact Us</li></a>
-            <a href='/generate'><li>Shorten</li></a>
             <li className='flex gap-3'>
                 <a href='/generate'><button className='bg-blue-700 rounded-xl shadow-lg p-3 py-1 font-bold'>Try Now</button></a>
                 <a href='/github'><button className='bg-blue-700  rounded-xl  py-1 font-boldshadow-lg p-3 py-1 font-bold'>Github</button></a>
